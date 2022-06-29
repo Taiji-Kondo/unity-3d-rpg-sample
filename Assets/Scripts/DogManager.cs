a023eddf5c6b2fe47f2397d465cab62bff053f83
taiji <k.taiji1108@gmail.com>
2022-06-29 23:45:47 +0900

    feat: Create DogManager

---
+++ Assets/Scripts/DogManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogManager : MonoBehaviour
+{
+    private Animator _animator;
+    private static readonly int Attack = Animator.StringToHash("Attack");
+    private static readonly int MoveSpeed = Animator.StringToHash("MoveSpeed");
+
+    void Start()
+    {
+        _animator = GetComponent<Animator
+        >();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger(Attack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            _animator.SetFloat(MoveSpeed, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _animator.SetFloat(MoveSpeed, 0);
+        }
+    }
+}
