42c26fb9b5476d394dc5ba737d2f6935a20d32f6
taiji <k.taiji1108@gmail.com>
2022-06-30 23:01:05 +0900

    feat: Create Player move feature

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,16 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+    private float _x;
+    private float _z;
+    public float moveSpeed = 3;
+    
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        // Move by keydown
+        _x = Input.GetAxis("Horizontal");
+        _z = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    { 
+        // Move speed
+        _rigidbody.velocity = new Vector3(_x, 0, _z) * moveSpeed;
     }
 }
