ab2bb599d17b50a24626907db7edca0a9d708191
taiji <k.taiji1108@gmail.com>
2022-07-01 23:25:35 +0900

    feat: Create Attack animation

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,6 +10,7 @@ public class PlayerManager : MonoBehaviour
     private float _x;
     private float _z;
     private static readonly int MoveSpeed = Animator.StringToHash("MoveSpeed");
+    private static readonly int Attack = Animator.StringToHash("Attack");
     
     public float moveSpeed = 3;
 
@@ -24,6 +25,12 @@ public class PlayerManager : MonoBehaviour
         // Move by keydown
         _x = Input.GetAxis("Horizontal");
         _z = Input.GetAxis("Vertical");
+        
+        // Attack by keydown
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger(Attack);
+        }
     }
 
     private void FixedUpdate()
