35760e04828f1fd37c2a7ee5ea017a343f156c24
taiji <k.taiji1108@gmail.com>
2022-07-01 23:43:51 +0900

    feat: Fix animator behaviour bug

---
+++ Assets/Scripts/Player/PlayerAttackBehaviour.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAttackBehaviour : StateMachineBehaviour
+    {
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            // Change moveSpeed to zero
+            animator.GetComponent<PlayerManager>().moveSpeed = 0;
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        // public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        // {
+        //     
+        // }
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            // reset moveSpeed
+            animator.GetComponent<PlayerManager>().moveSpeed = 3;
+        }
+
+        // OnStateMove is called right after Animator.OnAnimatorMove()
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that processes and affects root motion
+        //}
+
+        // OnStateIK is called right after Animator.OnAnimatorIK()
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that sets up animation IK (inverse kinematics)
+        //}
+    }
+}
