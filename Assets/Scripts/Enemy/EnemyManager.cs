36ae1fe8340414eed8750fd5de1745dec6f071d2
taiji <k.taiji1108@gmail.com>
2022-07-03 00:12:17 +0900

    feat: Create EnemyIdleBehaviour

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -6,18 +6,24 @@ namespace Enemy
     public class EnemyManager : MonoBehaviour
     {
         private NavMeshAgent _agent;
+        private Animator _animator;
+        private static readonly int Distance = Animator.StringToHash("Distance");
 
         public Transform target;
-        
+
         void Start()
         {
             _agent = GetComponent<NavMeshAgent>();
             _agent.destination = target.position;
+            
+            _animator = GetComponent<Animator>();
         }
 
         void Update()
         {
             _agent.destination = target.position;
+            
+            _animator.SetFloat(Distance, _agent.remainingDistance);
         }
     }
 }
